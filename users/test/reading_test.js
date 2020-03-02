168aec2cedfdd3e78b3abf4a1d98583b996357d1
Martin Kretzschmar <martin@jp7.com.br>
2020-03-02 16:21:15 -0300

    Wrapping up lessons on MongoDB test tools

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,14 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let user;
+  let alex, joe, maria, zach;
 
   beforeEach((done) => {
-    user = new User({
-      name: 'Joe'
-    });
+    alex = new User({ name: 'Alex' });
+    joe = new User({ name: 'Joe' });
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
 
-    user.save()
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
       .then(() => done());
   })
 
@@ -18,6 +19,18 @@ describe('Reading users out of the database', () => {
       .then((users) => {
         assert(users[0]._id.toString() === user._id.toString());
 
+        done();
+      });
+  });
+
+  it('can skpi and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
         done();
       });
   });
